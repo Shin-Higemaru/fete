08cae136a216be48f2acac9532af8c6b4e638055
Shin-Higemaru <shijinboshi@hotmail.fr>
2019-06-25 12:17:05 +0200

    event

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Event;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController {
+    /**
+     * @return Response;
+     * @Route( "/", name="index")
+     */
+
+    public function index(){
+        $em= $this->getDoctrine()->getManager();
+        $events = $em->getRepository(Event::class)->findAll();
+        return $this->render("coco.html.twig", [ "toto"=>"titi", "nom"=>"everybody"]);
+    }
+}
---
+++ src/Entity/Event.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EventRepository")
+ */
+class Event
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=181)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dday;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDday(): ?\DateTimeInterface
+    {
+        return $this->dday;
+    }
+
+    public function setDday(\DateTimeInterface $dday): self
+    {
+        $this->dday = $dday;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
